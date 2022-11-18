7a9fac228b64c5201c2e4cb4372dfee7b9e8048f
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 16:38:02 +0800

    feat: create renderConditional func

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+//When user is loggedIn => just show <h1> in the page
+//When user isn't loggedIn => just show <form> in the page
 
+let isLoggedIn = false; // false OR true
+
+function renderConditional() {
+    if (isLoggedIn) {
+        return (
+            <form className="form">
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        )
+    } else {
+        return (<h1>Hello</h1>);
+    }
+}
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+        {renderConditional()}
     </div>
   );
 }
